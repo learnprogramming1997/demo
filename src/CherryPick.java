869c9bfcbd144091fe4bc13bad4200f2c5b4f1f4
learnprogramming1997 <thangjobmail@gmail.com>
2019-10-24 08:55:00 +0700

    [ddthang] add a line for the cherry-pick first

--- src/CherryPick.java
+++ src/CherryPick.java
@@ -3,7 +3,7 @@ public class CherryPick {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-
+		System.out.println("This line for the one commit to cherry-pick");
 	}
 
 }
