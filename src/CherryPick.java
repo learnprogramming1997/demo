e22b01db27f6251bd15a679b70094bbcdc93eac2
learnprogramming1997 <thangjobmail@gmail.com>
2019-10-24 09:22:32 +0700

    [ddthang] group commit

--- src/CherryPick.java
+++ src/CherryPick.java
@@ -4,6 +4,11 @@ public class CherryPick {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("This line for the one commit to cherry-pick");
+		
+		System.out.println("The first commit to test more than commit to cherry-pick");
+		System.out.println("The second commit to test more than commit to cherry-pick");
+		System.out.println("The third commit to test more than commit to cherry-pick");
+		System.out.println("The fourth commit to test more than commit to cherry-pick");
 	}
 
 }
