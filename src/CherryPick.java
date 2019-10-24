3ae1224581e71bf680f1898063c6417153ae3093
learnprogramming1997 <thangjobmail@gmail.com>
2019-10-24 09:01:52 +0700

    [ddthang] add one line for the first commit

--- src/CherryPick.java
+++ src/CherryPick.java
@@ -4,6 +4,9 @@ public class CherryPick {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("This line for the one commit to cherry-pick");
+		
+		System.out.println("The first commit to test more than commit to cherry-pick");
+		
 	}
 
 }
